a8c84df44350bd3b1a86156b1890adce44c0452b
5Daydreams <nelsonkossuga@gmail.com>
2022-04-26 03:46:35 +0200

    + I can now render a triangle :v ;

--- GordanOpenGL/main.cpp
+++ GordanOpenGL/main.cpp
@@ -2,7 +2,19 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-double global_time;
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(0.8,0.3,0.02,1.0);\n"
+"}\n\0";
 
 int main()
 {
@@ -12,6 +24,13 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		+0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		+0.0f, +0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+	};
 
 	// create a window
 	GLFWmonitor* fullscreen = NULL;
@@ -33,20 +52,69 @@ int main()
 	gladLoadGL();
 
 	// viewport size, as in, where we want to render at
-	glViewport(0,0, width, height);
+	glViewport(0, 0, width, height);
 
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// Vertex Buffer Object, and Vertex Array Object
+	GLuint VAO;
+	GLuint VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const int ValuesPerVertex = 3;
+	glVertexAttribPointer(0, ValuesPerVertex, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	// camera's clear flags as a solid color
-	glClearColor(0.07f,0.13f,0.17f,1.0f);
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
-	while(!glfwWindowShouldClose(window))
+	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+
+		const int startingIndex = 0;
+		const int vertexCount = 3;
+		glDrawArrays(GL_TRIANGLES, startingIndex, vertexCount );
+
 		glfwSwapBuffers(window);
 		// if this is not done, window will be unresponsive
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	// cleaning up window reference and glfw context
 	glfwDestroyWindow(window);
