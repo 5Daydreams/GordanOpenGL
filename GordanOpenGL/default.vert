78316bf4b92a07678aaaa626c7f434fd870ea1ca
5daydreams <nelsonkossuga@gmail.com>
2022-04-26 20:43:22 +0200

    + Added shader uniforms and AttribPointer support for VAO's;

--- GordanOpenGL/default.vert
+++ GordanOpenGL/default.vert
@@ -1,6 +1,21 @@
 #version 330 core
+
+// Vertex Pos
 layout (location = 0) in vec3 aPos;
+// Vertex Colors
+layout (location = 1) in vec3 aColor;
+
+// out means this will be passed to the frag shaders
+out vec3 color;
+
+// uniform is constant for all elements in this shader stage, and is shared between vert/frag
+uniform float scale;
+
 void main()
 {
-	gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);
+	// Outputs the positions/coordinates of all vertices
+	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	
+	// Assigns the Vertex Colors to the "color" out-vec3 (needs to be out to be passed between from vertex to fragment shaders)
+	color = aColor;
 }
--- GordanOpenGL/main.cpp
+++ GordanOpenGL/main.cpp
@@ -8,14 +8,15 @@
 #include"EBO.h"
 
 GLfloat verticesTri[] =
-{
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-	+0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-	+0.0f, +0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
-	-0.5f / 2, +0.5f * float(sqrt(3)) / 6, 0.0f,
-	+0.5f / 2, +0.5f * float(sqrt(3)) / 6, 0.0f,
-	+0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f
+{ //               COORDINATES                  /       COLORS          //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,   0.80f, 0.30f, 0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,   0.80f, 0.30f, 0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   1.00f, 0.60f, 0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,   0.00f, 0.45f, 0.77f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,   0.00f, 0.45f, 0.77f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,   0.20f, 0.30f, 0.80f  // Inner down
 };
+
 
 GLfloat verticesQuad[] =
 {
@@ -82,8 +83,10 @@ int main()
 	// Generates Element Buffer Object and links it to indices
 	EBO EBO1(indicesTri, sizeof(indicesTri));
 
-	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
+	// Links VBO attributes such as coordinates and colors to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
